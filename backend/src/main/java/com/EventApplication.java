4f2f65491306f263483ab472e241fd9849f4667b
Sugu4 <sulo4690@hotmail.com>
2025-10-16 15:00:25 +0200

    AnbindungErfolgreich

--- backend/src/main/java/com/EventApplication.java
+++ backend/src/main/java/com/EventApplication.java
@@ -17,4 +17,26 @@ public class EventApplication {
     public static void main(String[] args) {
         SpringApplication.run(EventApplication.class, args);
     }
+    @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowCredentials(true);
+        corsConfiguration.setAllowedOrigins(Arrays.asList(
+                "http://localhost:5500/", "http://127.0.0.1:5500"));
+        corsConfiguration.setAllowedHeaders(Arrays.asList("Origin",
+                "Access-Control-Allow-Origin", "Content-Type",
+                "Accept", "Authorization", "Origin, Accept", "X-Requested-With",
+                "Access-Control-Request-Method", "Access-Control-Request-Headers"));
+        corsConfiguration.setExposedHeaders(Arrays.asList("Origin", "Content-Type",
+                "Accept", "Authorization",
+                "Access-Control-Allow-Origin", "Access-Control-Allow-Origin",
+                "Access-Control-Allow-Credentials"));
+        corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT",
+                "DELETE", "OPTIONS", "HEAD"));
+        UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
+        urlBasedCorsConfigurationSource.registerCorsConfiguration("/**",
+                corsConfiguration);
+        return new CorsFilter(urlBasedCorsConfigurationSource);
+    }
+
 }
